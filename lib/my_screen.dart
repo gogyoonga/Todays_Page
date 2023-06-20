39ed0501fc0fbeea7fa3057c1b8a70b2f7864a53
GoGyoonGa <ouna2@naver.com>
2023-06-20 15:25:45 +0900

    2023-06-20

--- lib/my_screen.dart
+++ lib/my_screen.dart
@@ -5,13 +5,42 @@ class MyScreen extends StatelessWidget{
   Widget build(BuildContext context) {
     // TODO: implement build
     return Column(children: [Container(
-      color: Colors.green[700],
-      height: 100,
-    ),
-      Text('My에는 다이어리가 떠요'),
-      Container(
-        color: Colors.amber[300],
+      color: Colors.green[100],
+      height: 130,
+      child: Row(children: [Container(
+        width: 100,
         height: 100,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          shape: BoxShape.circle,
+        ),
+      ),
+        Container(
+          width: 200,
+          height: 100,
+          color: Colors.white,
+          child: Text('꼬모냥냥\n 발자국: 500 \n클로버: 30'),
+        )],
+      ),
+    ),
+      Container(
+        width: 320,
+        height: 460,
+        decoration: BoxDecoration(
+            color: Colors.amber[100],
+            borderRadius: const BorderRadius.only(
+                topRight: Radius.circular(50),
+                topLeft: Radius.circular(10),
+                bottomRight: Radius.circular(50),
+                bottomLeft: Radius.circular(10)
+            ),
+            image: const DecorationImage(
+                image: AssetImage(
+                    ''
+                )
+            )
+        ),
+
       ),
     ],
     );
